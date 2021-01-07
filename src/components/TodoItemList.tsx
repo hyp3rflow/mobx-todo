4cbbfdb472678717b8c9d2dbfb0edd14e6b07e35
hyperflow <hyperflow@kakao.com>
2021-01-07 13:56:03 +0900

    feat(TodoItemList): pinned item은 상단에 노출되도록 추가

--- src/components/TodoItemList.tsx
+++ src/components/TodoItemList.tsx
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { observer } from "mobx-react";
 
-import TodoStore from "../store/TodoStore";
+import TodoStore, { ITodo } from "../store/TodoStore";
 import TodoItem from "./TodoItem";
+import { autorun } from "mobx";
 
 const TodoItemList = () => {
   const todoList = TodoStore.instance.getTodoList;
+  const [tempList, setTempList] = useState<ITodo[]>([]);
+
+  useEffect(
+    () =>
+      autorun(() => {
+        setTempList(
+          todoList.slice().sort((a, b) => {
+            if (a.pinned && !b.pinned) return -1;
+            else if (!a.pinned && b.pinned) return 1;
+            else return 0;
+          })
+        );
+      }),
+    []
+  );
 
   return (
-    <div>
-      {todoList.map((todo) => (
+    <Container>
+      {tempList.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  margin: 30px auto;
+  width: 500px;
+  min-height: 200px;
+
+  display: flex;
+  flex-direction: column;
+
+  border-radius: 15px;
+  background: #ffffff;
+  box-shadow: 20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff;
+`;
 
 export default observer(TodoItemList);
